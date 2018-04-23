6c334eaffc72f7edcb2e2023a7e9cc64f9b8706d
Ausar McGruder <ausar_mcgruder@yahoo.com>
2018-04-23 19:56:05 -0400

    Final Commit 4/23/18

--- portfolio/js/init.js
+++ portfolio/js/init.js
@@ -4,5 +4,15 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+// Projects section slide show
+    $(document).ready(function(){
+       $('.slider').slider();
+     });
+
+// Expandable images
+     $(document).ready(function(){
+       $('.materialboxed').materialbox();
+     });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
